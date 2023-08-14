8f9fa040ffc1724fc19b71dd67f6560672e98543
Psagrox <waljaviergalvan@gmail.com>
2023-08-14 19:49:44 -0300

    update (code improvements)

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 //The user generate an input, which must be Rock, Paper or Scissors
 document.getElementById("boton").addEventListener("click",function(){
     let opcion_usuario = prompt("¿que opcion queres? 1.Rock 2.Paper 3.Scissors");
@@ -54,33 +55,97 @@ document.getElementById("boton").addEventListener("click",function(){
     }else if (opcionUsuario == "Rock" && opcionAleatoria == "Paper"){
         //If the User choose Rock and computer choose paper computer wins
         console.log("Computer Wins");
-        scoreComputer.textContent = scoreComputer ++;
+        scoreComputer.textContent = parseInt(scoreComputer.textContent) +1;
 
     }else if (opcionUsuario == "Rock" && opcionAleatoria == "Scissors"){
         //IF the User choose Rock and computer choose scissors User Wins
         console.log("User Wins");
-        scoreUser.textContent = scoreUser ++;
+        scoreUser.textContent = parseInt(scoreUser.textContent) +1;
 
     }else if (opcionUsuario == "Paper" && opcionAleatoria == "Scissors"){
         //If the User choose Paper and computer choose scissors computer wins
         console.log("Computer Wins");
+        scoreComputer.textContent = parseInt(scoreComputer.textContent) +1;
+
     }else if (opcionUsuario == "Paper" && opcionAleatoria == "Rock"){
         //IF the User choose Paper and computer choose Rock User Wins
         console.log("User Wins");
+        scoreUser.textContent = parseInt(scoreUser.textContent) +1;
+
     }else if (opcionUsuario == "Scissors" && opcionAleatoria == "Rock"){
         //If the User choose scissors and computer choose Rock computer wins
         console.log("Computer Wins");
+        scoreComputer.textContent = parseInt(scoreComputer.textContent) +1;
+
     }else {
         //If the User choose scissors and computer choose Rock computer wins
         console.log("User Wins");
+        scoreUser.textContent = parseInt(scoreUser.textContent) +1;
     }
 
 
 })
+*/
 
+//I get the references of the buttons
+var rockButton = document.getElementById("rockButton");
+var paperButton = document.getElementById("paperButton");
+var scissorsButton = document.getElementById("scissorsButton");
 
+//I get the references of the elements of the score in the HTML
+var scoreUser = document.getElementById("userScore");
+var scoreComputer = document.getElementById("computerScore");
 
+//Function to update the score
+var userChoice;
+var randomChoice;
+function updateScore (userChoice, randomChoice){
+    if (userChoice == randomChoice){
+        console.log("Draft");
+    } else if (
+        (userChoice == "Rock" && randomChoice == "Scissors") ||
+        (userChoice == "Paper" && randomChoice == "Rock") ||
+        (userChoice == "Scissors" && randomChoice == "Paper")
+    ) {
+        console.log("User Wins");
+        scoreUser.textContent = parseInt(scoreUser.textContent) + 1;
+    } else {
+        console.log("Computer Wins");
+        scoreComputer.textContent = parseInt(scoreComputer.textContent) + 1;
+    }
+}
 
+//Assign events of clicks of the buttons
+rockButton.addEventListener("click", function(){
+    var userChoice = "Rock";
+    var randomChoice = computerChoice(options);
+    console.log("You choose: " + userChoice);
+    console.log("Computer choose: " + randomChoice);
+    updateScore(userChoice, randomChoice);
+});
+
+scissorsButton.addEventListener("click", function(){
+    var userChoice = "Scissors";
+    var randomChoice = computerChoice(options);
+    console.log("You choose: " + userChoice);
+    console.log("Computer choose: " + randomChoice);
+    updateScore(userChoice, randomChoice);
+});
+
+paperButton.addEventListener("click", function(){
+    var userChoice = "Paper";
+    var randomChoice = computerChoice(options);
+    console.log("You choose: " + userChoice);
+    console.log("Computer choose: " + randomChoice);
+    updateScore(userChoice, randomChoice);
+});
+
+//Function to generate randomChoice
+var options = ["Rock", "Paper", "Scissors"];
+
+function computerChoice(options){
+    return options[Math.floor(Math.random() * options.length)];
+}
 
 
 
